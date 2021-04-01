494b35f18e4474e05f0034e6598f54e44eb111de
Dustin <DustinTiner@Emilys-MacBook-Air.local>
2021-03-31 21:28:14 -0500

    display specified site data

--- src/components/Manager.js
+++ src/components/Manager.js
@@ -1,43 +1,80 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState,Component} from 'react'
 import axios from 'axios'
+import {Link,Route} from 'react-router-dom'
+import SitePage from "./SitePage"
 
 
-function Manager () {
-
-    const [campsites,useCampsites] = useState(null)
-    const campArr = []
-    const finalList = []
-    let unique =[]
-    
-    const dbURL = process.env.DB_URL || "https://campsite-manager.herokuapp.com/form"
-    
-   axios.get(dbURL)
-        .then(res => {campArr.push(res.data)})
-        .catch(err => console.log(err))
-
-    async function getCamps() {
-        campArr[0].map(item => {
-        finalList.push(item.location)
-         })        
-        unique = [...new Set(finalList)]
-          console.log('uniquie',unique)
+class Manager extends Component {
+    constructor(){
+        super();
+        this.state = {
+            siteArr:[]
+        };
+        this.url = process.env.DB_URL || "https://campsite-manager.herokuapp.com/form"
+        this.getSites = this.getSites.bind(this);
+        this.getNo = this.getNo.bind(this);
     }
 
+    componentDidMount(){
+        this.getSites();
+    }
 
+    async getSites() {
+        const campArr = []
 
-    return(
+        let data = await axios.get(this.url)
+        // original
+        .then(res => res.data.map(site => campArr.push(site.location)))
+        .catch(err => console.log(err))
 
-        <>
-        <h1>Manager Page</h1>
-        <div>
-        {unique.map(siteNo => {
-            <p>{siteNo}</p>
-        })}
-        </div>
+        this.state.siteArr = Array.from([...new Set(campArr)])
 
-        <button onClick={getCamps} >get campsites</button>
-        </>
-    )
+        console.log("siteArr",this.state.siteArr)
+        this.forceUpdate()
+        // this.state.siteArr.map(item => console.log(item))
+    }
+
+    getNo (e) {
+        console.log(e)
+    }
+
+    render(){
+        const {siteArr} = this.state;
+        console.log("sitearr",siteArr)
+        return(
+
+            <>
+            <h1>Manager Page</h1>
+             {siteArr && siteArr.map(item => {
+                return(
+                   <Link onClick={this.getNo(item)} to={"/manager/" + item}> {item} </Link> 
+                )
+                })} 
+                <Route path={"/manager"}>
+                    <SitePage />
+                </Route>
+            </>
+        )        
+    }
 }
 
 export default Manager
+
+
+
+//     const [campsites,useCampsites] = useState(null)
+//     const campArr = []
+//     const finalList = []
+//     let unique =[]
+    
+    // const dbURL = process.env.DB_URL || "https://campsite-manager.herokuapp.com/form"
+    
+
+
+//     async function getCamps() {
+//         campArr[0].map(item => {
+//         finalList.push(item.location)
+//          })        
+//         unique = [...new Set(finalList)]
+//           console.log('uniquie',unique)
+//     }
---
+++ src/components/SitePage.js
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react"
+import React, {Component} from "react"
+
+class SitePage extends Component {
+    constructor(){
+    super()
+    }
+
+
+render(){
+    return(
+        <p>hey</p>
+    )
+    }
+}
+export default SitePage
